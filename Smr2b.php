4a6a5c1131e294792f1c73f31609d1e98097c538
Caampo03 <davidajocr92@gmail.com>
2019-10-01 17:39:37 +0200

    formulario

--- Smr2b.php
+++ Smr2b.php
@@ -3,10 +3,25 @@
 David
 <a href="http://192.168.0.107/smr2b_mario/smr2b.php">Mario</a>
 
+<form>
+	<input name="email" type="email">
+	<button>Enviar</button>
+</form>
+
+
 <?php
 $f=fopen('visitas.txt','a');
 fwrite($f,date('Y-m-d H:i:s '));
 fwrite($f,$_SERVER['REMOTE_ADDR']);
 fwrite($f,"\r\n");
 fclose($f)
+?>
+
+<?php
+if(isset($_GET['email'])){
+	$f=fopen('suscriptores.txt','a');
+	fwrite($f,$_GET['email']);
+	fwrite($f,"\r\n");
+	fclose($f);
+}
 ?>
