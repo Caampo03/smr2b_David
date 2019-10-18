b3d94cbf7b4981b40edd9a8d42c5754dfef5d32b
Caampo03 <davidajocr92@gmail.com>
2019-10-18 18:16:13 +0200

    html.php

---
+++ html.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+	<title>david</title>
+</head>
+<body>
+<font face="arial,verdana">
+	<h1>AJO</h1>
+	<font color="red">Pueblo de 2.000 personas</font>
+	<br/>
+	<h2>Vivo yo</h2>
+	<font color="green">David Campo <b>alcoholico anonimo</b></font>
+	<br/><img src="uno.jpg"/>
+	<br/>
+	<h3>Tiene tres playas y ria</h3>
+	<u>Antuerta</u>, Cuberris y Sorrozuela
+	<font size=5>Ria de ISLA/AJO</font>
+	<br><br/>
+	
+	<center>
+	<table border>
+	<tr><td>autonomia</td><td>provincia</td></tr>
+	<tr><td>Galicia</td><td>LaCoruña</td></tr>
+	<tr><td>Galicia</td><td>Ourense</td></tr>
+	<tr><td>Cantabria</td><td>Cantabria</td></tr>
+	<tr><td>Asturias</td><td>Asturias</td></tr>
+	<tr><td>Pais Vasco</td><td>Gipuzkoa</td></tr>
+	</table>	
+	</center>
+	
+	<form>
+	
+	<input type="text">
+	<input type="submit">
+</font>
+</body>
+</html>
