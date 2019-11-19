7585ca4aa87b2e1b8aa00d5b051f355a05884d8e
Caampo03 <davidajocr92@gmail.com>
2019-11-19 16:50:51 +0100

    css_style

---
+++ css_style.php
@@ -0,0 +1,59 @@
+<html>
+<head>
+	<title>david</title>
+	<style>
+body{
+	font-family: arial, verdana;	
+}	
+.rojo{
+	color:red;
+}
+.verde{
+	color:green;
+}
+.underline{
+	text-decoration:underline;
+}
+.fontsize{
+	font-size:1.5em;
+}
+.border{
+	border: 1px solid black;
+}
+
+
+
+	</style>
+</head>
+<body>
+	<h1>AJO</h1>
+	<span class="rojo">Pueblo de 2.000 personas</span>
+	<br/>
+	<h2>Vivo yo</h2>
+	<span class="verde">David Campo <span style="font-weight:bold;">alcoholico anonimo</span></span>
+	<br/><img src="uno.jpg"/>
+	<br/>
+	<h3>Tiene tres playas y ria</h3>
+	<span class="underline">Antuerta</span>, Cuberris y Sorrozuela
+	<span class="fontsize">Ria de ISLA/AJO</span>
+	<br><br/>
+	
+	<center>
+	<table class="
+		border
+	">
+	<tr><td>autonomia</td><td>provincia</td></tr>
+	<tr><td>Galicia</td><td>LaCoruña</td></tr>
+	<tr><td>Galicia</td><td>Ourense</td></tr>
+	<tr><td>Cantabria</td><td>Cantabria</td></tr>
+	<tr><td>Asturias</td><td>Asturias</td></tr>
+	<tr><td>Pais Vasco</td><td>Gipuzkoa</td></tr>
+	</table>
+	</center>
+	
+	<form>
+	
+	<input type="text">
+	<input type="submit">
+</body>
+</html>
